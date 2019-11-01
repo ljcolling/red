8d17fed8898eeb155577f5e776d42858df791736
Lincoln <lincoln@colling.net.nz>
2019-11-01 17:37:51 +0000

    updated

---
+++ R/functions.R
@@ -0,0 +1,10 @@
+#' Say Colour
+#'
+#' This function says the colour.
+#' What colour is says depends on the package.
+#' If it's the blue package then it says 'blue'.
+#' If it's the red package then it says 'red'.
+#' @export
+saycolour <- function(){
+  cat("This is the 'saycolour()' function from the 'red' package")
+}
